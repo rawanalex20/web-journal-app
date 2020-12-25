c47719fdc19a67a5001023c75e3f310caa7f975e
rawanalex20 <63899074+rawanalex20@users.noreply.github.com>
2020-12-25 19:49:11 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,48 +1,70 @@
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+/* API credentials */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=';
+const apiKey = '72265d737e3d151aa7160a92ffc0a15e';
 
-let baseURL = '';
-let apiKey = '';
+/* eventlistner with function of series of actions*/
+document.getElementById('generate').addEventListener('click', performAction);
 
-document.getElementById('zip').addEventListener('click', performAction);
-
-function performAction(e){
-const newData =  document.getElementById('').value;
-getAnimal(baseURL,newData, apiKey)
-
+/* perform a chain of get, post requests and update */
+function performAction(){
+  const zipCode = document.getElementById('zip').value;
+  getWeather(baseURL, apiKey)
+  .then(function(data){
+    postData('/addData',{
+      temperature: data.main.temp,
+      date: new Date(),
+      userResponse: zipCode})
+    .then(updateUI('/getData'));
+  })
 }
-const getAnimal = async (baseURL, weatherData, key)=>{
+  
 
-  const res = await fetch(baseURL+weatherData+key)
+/* get request function for weather data */
+const getWeather = async (baseURL, apiKey)=>{
+  const res = await fetch(baseURL + apiKey)
   try {
-
     const data = await res.json();
-    console.log(data)
     return data;
-  }  catch(error) {
-    console.log("error", error);
+  }
+  catch(error) {
+    console.log('get error ', error);
   }
 }
 
+/* post request function for weather data */
 const postData = async ( url = '', data = {})=>{
-  console.log(data);
     const response = await fetch(url, {
     method: 'POST', 
     credentials: 'same-origin',
     headers: {
         'Content-Type': 'application/json',
-    },
-       
+    },      
     body: JSON.stringify(data), 
   });
 
     try {
       const newData = await response.json();
-      console.log(newData);
       return newData;
     }catch(error) {
-    console.log("error", error);
+    console.log("post error: ", error);
     }
 }
 
-postData('/add', {});
+
+
+// get request to update 
+const updateUI = async (url) => {
+  const request = await fetch(url);
+  console.log('request', request)
+  try{
+    const allData = await request.json();
+    const date = new Date();
+    document.getElementById('temp').innerHTML = allData.temperature;
+    document.getElementById('date').innerHTML = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`;
+    document.getElementById('content').innerHTML = allData.userResponse;
+    console.log('try', allData)
+  
+  }catch(error){
+    console.log("updateUI error", error);
+  }
+  }
