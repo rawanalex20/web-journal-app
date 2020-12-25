5fd253b5b5b711fc0e62fd2483fa5ffa463d0c14
rawanalex20 <63899074+rawanalex20@users.noreply.github.com>
2020-12-25 19:47:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
 
+/* Express to run server and routes */
 const express = require('express');
+/* Start instance of app */
 const app = express();
 
-const bodyParser = require('body-parser')
+/* Dependencies */
+/* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const cors = require('cors');
+// Cors for cross origin allowance
 app.use(cors());
 
+/* Initializing the main project folder */
 app.use(express.static('website'));
 
-app.get('/', function (req, res) {
-    res.send('server running');
-});
-
-app.use(function(req, res, next) {
-    res.status(404).send("404");
-});
-
-const port = 8080;
-
+// Configure and start server
+const port = 8000;
 const server = app.listen(port, ()=>{console.log(`server running on port: ${port}`)});
+
+
+/* post route on '/addData' path */
+app.post('/addData', addData);
+
+function addData(req, res){
+  projectData = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    userResponse: req.body.userResponse
+  };
+}
+
+/* post route on '/getData' path */
+app.get('/getData', (req, res)=>{
+  res.send(projectData);
+})
 
 
